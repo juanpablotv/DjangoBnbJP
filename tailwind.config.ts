e47b41750c080db87c9ef6225c514f83e609cb57
juanpablotv <torresvaldesjuanpablo@gmail.com>
2025-03-24 11:11:05 -0600

    02 dev a navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,21 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    ".app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages//*.{js,ts,jsx,tsx,mdx}",
+    "./components//*.{js,ts,jsx,tsx,mdx}",
+    "./app//*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         'airbnb': '#ff385c',
-        'airbnb-dark': '#d50027',
-
+        'airbnb-dark': '#d50027'
       }
     },
   },
   plugins: [],
 };
 export default config;
-
-
